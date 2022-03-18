5ba70cc4b67cd75c5f4d113a8ad4db6428883e11
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-03-18 23:09:30 +0530

    update main.js

--- static/JavaScript/main.js
+++ static/JavaScript/main.js
@@ -1,3 +1,6 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
+
+alert("delivering hearts to my followers..❣️");
+alert("loading animation... plz wait.");

94940ea4123df5b5bb2cc903324f83adf4295ce2
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-03-19 20:31:04 +0530

    update main.js

--- static/JavaScript/main.js
+++ static/JavaScript/main.js
@@ -2,5 +2,9 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
+function SumOfNumbers() {
+  document.getElementById("SumExam").innerHTML = 2+5;
+}
+
 alert("delivering hearts to my followers..❣️");
 alert("loading animation... plz wait.");

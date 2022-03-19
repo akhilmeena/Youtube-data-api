ccc82b736c76aaa0726be73463685bb586d2e611
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-03-20 00:37:18 +0530

    update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
 def about():
   path = "static/Data/Data.json"
   data = get_static_json(path)
-  return data
+  return JSON.stringify(data, undefined, 2)
 
 def get_static_json(path):
   with open(path, 'r') as f:

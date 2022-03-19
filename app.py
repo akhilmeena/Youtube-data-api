290b276c7a29723a76bcacdfc944798d2478da38
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-03-19 23:58:47 +0530

    update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 import json
 app = Flask(__name__)
 
-def get_static_json(path):
-    return json.load(open(get_static_file(path)))
-
-
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -15,6 +11,11 @@ def about():
   path = "static/Data/Data.json"
   data = get_static_json(path)
   return data
+
+def get_static_json(path):
+    return json.load(open(get_static_file(path)))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)

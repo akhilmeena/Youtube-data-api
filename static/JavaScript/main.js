f4ac7f17c9b1f44dd228a87df36cca03e9759139
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-03-19 22:35:25 +0530

    update main.js

--- static/JavaScript/main.js
+++ static/JavaScript/main.js
@@ -6,14 +6,5 @@ function SumOfNumbers() {
   document.getElementById("SumExam").innerHTML = 2+5;
 }
 
-function goPython(){
-  $.ajax({
-  url: "/scripts/file.py",
-  context: document.body
-  }).done(function() {
-    alert('finished python script');;
-  });
-}
-
 //alert("delivering hearts to my followers..❣️");
 alert("loading animation... plz wait.");
